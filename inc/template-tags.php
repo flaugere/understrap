066bfe625e1d96397d5a9cd07293bb10f4bd54b2
David FLAUGERE <flaugere@gmail.com>
2016-10-09 05:54:45 +0200

    Pas d'hyperlien dans les métas d'un article

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -21,11 +21,11 @@ function understrap_posted_on() {
 	);
 	$posted_on = sprintf(
 		esc_html_x( 'Posted on %s', 'post date', 'understrap' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		$time_string
 	);
 	$byline = sprintf(
 		esc_html_x( 'by %s', 'post author', 'understrap' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		'<span class="author vcard">' . esc_html( get_the_author() ) . '</span>'
 	);
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 }
